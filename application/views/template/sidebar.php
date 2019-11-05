2d6bcba6b9a5be4bffd190ea632bdaef7ac43036
ahmad-choirul <31815400+ahmad-choirul@users.noreply.github.com>
2019-11-05 20:00:46 +0700

    update

--- application/views/template/sidebar.php
+++ application/views/template/sidebar.php
@@ -70,7 +70,7 @@
                     <i class="fa fa-dashboard"></i> <span> Nilai Setpoint</span></i>
                 </a>
             </li> 
-            <li class="treeview">
+<!--             <li class="treeview">
                 <a href="#">
                     <i class="fa fa-edit"></i> <span>Hitung Manual</span>
                     <i class="fa fa-angle-left pull-right"></i>
@@ -85,7 +85,7 @@
                 <a href="<?php echo site_url('Csimulasi') ?>">
                     <i class="fa fa-envelope"></i> <span>Simulasi</span></i>
                 </a>
-            </li>
+            </li> -->
         </ul>
     </section>
     <!-- /.sidebar -->
